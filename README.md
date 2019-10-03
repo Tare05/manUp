d399c9f51369d13162019361f40698dc4116a1db
tare <taba.imre@silentsignal.hu>
2019-10-03 16:41:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+manUp (or at least Google it)
+=============================
+
+This ghidra script will open the manual page of the function found at the current selected address.
+If manual page cannot be found for the specific function, the script will initiate a search query to Google.
+
+Usage/Customize
+---------------
+
+Change the *BROWSER* and the *TERMINAL* variables at the beginning of the script to your preferred browser/terminal emulator.
+
+By default the *SHIFT-G* hotkey is used for the lookup, you can change it to whatevs at the header section of the script.
+
+Thats all you have to be careful of, just add it to your ghidra scripts and you can open up those man's faster then Jason.
---
+++ manUp.py
@@ -0,0 +1,39 @@
+# This script will look up function names found at the currently selected address and opens a man page to it.
+# If man page cannot be found, it will automatically start a google query for the function.
+# Change BROWSER and TERMINAL variables accordindly to your system/preferences.
+
+#@author 
+#@category Helper
+#@keybinding SHIFT-G
+#@menupath 
+#@toolbar 
+
+BROWSER  = 'firefox'
+TERMINAL = 'st'
+GOOGLE_SEARCH_PREFIX = "https://www.google.com/search?btnI&q="
+DUCKDUCKGO_SEARCH_PREFIX = "https://duckduckgo.com/?q=!ducky "
+
+from subprocess import Popen
+
+def lookUpFunction():
+	for r in getReferencesFrom(currentAddress):
+		if r.isExternalReference():
+			func_name = r.getLibraryName() + " " + r.getLabel()
+			break
+		else:
+			func_name = getFunctionAt(r.getToAddress())
+			if func_name:
+				return func_name
+				break
+
+func_name = lookUpFunction()
+
+if func_name == None:
+	print("Failed to get function name from current address.")
+else:
+	handle = Popen(["man", str(func_name)])
+	ret = handle.wait()
+	if (ret == 0):
+		Popen([TERMINAL, "man", str(func_name)])
+	else:
+		Popen([BROWSER, GOOGLE_SEARCH_PREFIX+str(func_name)])
